3dbf8209a4d83fca9ed6d0b9c9f734471833b19c
NadineWafaa <85240926+NadineWafaa@users.noreply.github.com>
2023-06-13 17:27:49 +0300

    chat icon

--- Graduation-Project/src/views/HomeView.vue
+++ Graduation-Project/src/views/HomeView.vue
@@ -39,6 +39,20 @@
          </div>
        </div>
      </div>
+
+     <!--ChatBot-->
+     <button class="open-button" @click="openForm()"><i class="far fa-comment" ></i></button>
+     <div class="chat-popup" id="myForm">
+        <form action="/action_page.php" class="form-container">
+          <h5>Welcome, How can we help you?</h5>
+
+          <label for="msg"><b>Message</b></label>
+          <textarea placeholder="Type message.." name="msg" required></textarea>
+
+          <button type="submit" class="btn">Send</button>
+          <button type="button" class="btn cancel" @click="closeForm()">Close</button>
+        </form>
+      </div>
      <Footer/>
 </template>
 
@@ -48,6 +62,8 @@ body{
  margin: 0;
  font-family: "Roboto", sans-serif;    
  background-color: var(--background);
+ box-sizing: border-box;
+ 
 }
 .homepage{
  height:645.25px;
@@ -125,12 +141,89 @@ body{
 }
 
 .b1:hover ,
-.b1:active{
- color: var(--darkblue) !important;
- font-weight: 700 !important;
- cursor: pointer;
-
+.b1:active,
+.open-button:hover,
+.open-button:active{
+  color: var(--darkblue) !important;
+  font-weight: 700 !important;
+  cursor: pointer;
 }
+
+
+
+.open-button {
+  border-radius: 50px;
+  background-color: var(--lightblue);
+  color: var(--light);
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  position: fixed;
+  bottom: 23px;
+  right: 28px;
+  width: 50px;
+  height: 50px;
+  
+}
+
+/* The popup chat - hidden by default */
+.chat-popup {
+  display: none;
+  position: fixed;
+  bottom: 0;
+  right: 15px;
+  border: 3px solid #f1f1f1;
+  z-index: 9;
+}
+
+/* Add styles to the form container */
+.form-container {
+  max-width: 300px;
+  padding: 10px;
+  background-color: white;
+}
+
+/* Full-width textarea */
+.form-container textarea {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  border: none;
+  background: #f1f1f1;
+  resize: none;
+  min-height: 200px;
+}
+
+/* When the textarea gets focus, do something */
+.form-container textarea:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+/* Set a style for the submit/send button */
+.form-container .btn {
+  background-color: green;
+  color: white;
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  margin-bottom:10px;
+  opacity: 0.8;
+}
+
+/* Add a red background color to the cancel button */
+.form-container .cancel {
+  background-color: red;
+}
+
+/* Add some hover effects to buttons */
+.form-container .btn:hover, .btn:active {
+  color: var(--darkblue) !important;
+  font-weight: 700 !important;
+  cursor: pointer;
+}
+
 </style>
 
 <script>
@@ -140,6 +233,20 @@ import Footer from '../components/Footer.vue'
    name: "HomeView",
    components:{
     NavBar , Footer
+  },
+  methods:{
+    openForm() {
+      document.getElementById("myForm").style.display = "block";
+},
+
+closeForm() {
+    document.getElementById("myForm").style.display = "none";
+} 
+    
+    
+
+  
+
   }
    
 }
