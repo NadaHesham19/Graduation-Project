6662c448634fd3f63236c78aa13399cc50220852
farahmrwan16333 <farahmrwan33@gmail.com>
2023-06-21 21:29:58 +0300

    info+maps

--- Graduation-Project/src/views/SpaceView.vue
+++ Graduation-Project/src/views/SpaceView.vue
@@ -28,13 +28,16 @@
                         <li class="align-items-center m-3 fs-4 "><i class="fa-solid fa-dollar-sign me-3"></i>
                             {{ spaceDetails.minPrice }} - {{ spaceDetails.maxPrice }}
                         </li>
-                        <li class="align-items-center m-3 fs-4 "><i class="fa-solid fa-bell me-3"></i>
-                            Services is here
+                        <li class="align-items-center m-3 fs-4 "><i class="fa-solid fa-mug-saucer me-3"></i>
+                            Drinks: {{ displayBooleanValue(spaceDetails.drinks) }}
+                        </li>
+                        <li class="align-items-center m-3 fs-4 "><i class="fa-solid fa-tree me-3"></i>
+                            Outdoors: {{ displayBooleanValue(spaceDetails.outdoors) }}
                         </li>
                     </ul>
                 </div>
                 <div class="col-6 mb-5">
-                    <img class="img-fluid image mt-5 " :src="firstImgSrc" alt="" width="520">
+                    <img class="img-fluid image mt-5 " :src="imageSrc" alt="" width="520">
                 </div>
             </div>
             <div class="row mt-5 pt-5">
@@ -86,32 +89,35 @@ export default {
             spaceDetails: {},
             latitude: null,
             longitude: null,
+            location: [],
             map: null,
             marker: null,
             rooms: [],
             currentPage: 1,
             roomsPerPage: 3,
-            searchTerm: ''
+            searchTerm: '',
+            imageSrc: "",
         }
     },
     components: { NavBar, RoomCard, Footer },
-    // created() {
-    //     this.spaceId = this.$route.params.id;
-    // },
+
     mounted() {
-        this.latitude = 30.06436416981507;   //parseFloat(lat)
-        this.longitude = 31.27810362252527;
-        // 30.065914025055683, 31.282058295558517
+        // this.latitude = this.location[0];   //parseFloat(lat)
+        // this.longitude = this.location[1];
+        // console.log(this.latitude, "mounted")
+        // console.log(this.longitude, "mounted")
 
-        this.map = new google.maps.Map(document.getElementById('map'), {
-            center: { lat: this.latitude, lng: this.longitude },
-            zoom: 12
-        });
+        // this.map = new google.maps.Map(document.getElementById('map'), {
+        //     center: { lat: this.latitude, lng: this.longitude },
+        //     zoom: 12
+        // });
 
-        this.marker = new google.maps.Marker({
-            position: { lat: this.latitude, lng: this.longitude },
-            map: this.map
-        });
+        // this.marker = new google.maps.Marker({
+        //     position: { lat: this.latitude, lng: this.longitude },
+        //     map: this.map
+        // });
+
+        this.fetchImage();
     },
     beforeMount() {
         axios
@@ -132,11 +138,23 @@ export default {
             .catch((err) => {
                 console.error(err);
             });
+        axios
+            .get(`http://localhost:8080/api/spaces/getCoordinates/?spaceId=${this.spaceId}`)
+            .then((response) => {
+                console.log(response.data, "location")
+                this.location = response.data;
+                this.latitude = this.location[0];
+                this.longitude = this.location[1];
+                this.initializeMap();
+
+
+            })
+            .catch((err) => {
+                console.error(err);
+            });
     },
     computed: {
-        firstImgSrc() {
-            return this.spaceDetails.images;
-        },
+
         totalPages() {
             const filteredRooms = this.rooms.filter(room => {
                 const searchTerm = this.searchTerm.toLowerCase();
@@ -159,10 +177,38 @@ export default {
             return filteredRooms.slice(startIndex, endIndex);
         },
     }, methods: {
+        initializeMap() {
+            this.map = new google.maps.Map(document.getElementById('map'), {
+                center: { lat: this.latitude, lng: this.longitude },
+                zoom: 12
+            });
+
+            this.marker = new google.maps.Marker({
+                position: { lat: this.latitude, lng: this.longitude },
+                map: this.map
+            });
+        },
         search() {
             this.currentPage = 1;
-        }
-    }
+        },
+        displayBooleanValue(value) {
+            return value ? "Yes" : "No";
+        },
+        fetchImage() {
+            axios
+                .get(`http://localhost:8080/api/images/space/${this.spaceId}/0`, {
+                    responseType: "arraybuffer",
+                })
+                .then((response) => {
+                    const blob = new Blob([response.data], { type: "image/jpeg" }); // Create a Blob 
+                    this.imageSrc = URL.createObjectURL(blob); //  URL for the Blob
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        },
+    },
+
 }
 </script>
 
